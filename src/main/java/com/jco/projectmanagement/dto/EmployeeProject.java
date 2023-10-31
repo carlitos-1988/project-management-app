62daf87a8aefe781adf2f1255176e140c78e0675
Juan C Olmedo <doubleparked88@gmail.com>
2023-10-30 22:59:03 -0700

    choosing to stop here for now employee repo generated new query with left join for displaying how many projects each employee has.  generation of SQL querie might need additional work, still need to implement view of new dto query object created

---
+++ src/main/java/com/jco/projectmanagement/dto/EmployeeProject.java
@@ -0,0 +1,10 @@
+package com.jco.projectmanagement.dto;
+
+public interface EmployeeProject {
+
+    //DAO needs to have the property names begin with GET
+    public String getFirstName();
+    public String getLastName();
+    public int getProjectCount();
+
+}
